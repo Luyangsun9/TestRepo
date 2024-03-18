a8af961b9b540945c3881f6cf3ce26c8328b2ce7
Luyangsun9 <48081217+Luyangsun9@users.noreply.github.com>
2024-03-18 10:15:12 -0700

    Update HelloWorld.cs
    Added try catch

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -4,11 +4,19 @@ class Program
 {
     static void Main()
     {
-        // path of the file that we want to create
-        string pathName = @"C:\Program\myFile.txt";
+        try
+        {
+            // path of the file that we want to create
+            string pathName = @"C:\Program\myFile.txt";
 
-        // Create() creates a file at pathName 
-        FileStream fs = File.Create(pathName);
+            // Create() creates a file at pathName 
+            FileStream fs = File.Create(pathName);
+        }
+        catch( exception ex)
+        {
+            MessageBox.Show($"{ex.Message}");
+        }
+
 
         // check if myFile.txt file is created at the specified path 
         if (File.Exists(pathName))
